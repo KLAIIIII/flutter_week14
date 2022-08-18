fe9c71064901efa18f93ca70a3620a6bc43893c1
KLAIIIII <guyyg03546@gmail.com>
2022-08-18 17:20:59 +0700

    update

--- lib/app.dart
+++ lib/app.dart
@@ -3,6 +3,7 @@ import 'contact.dart';
 import 'home.dart';
 import 'list_view_builder.dart';
 import 'list_view_demo.dart';
+import 'list_view_menu.dart';
 import 'list_view_separate.dart';
 
 class MyApp extends StatelessWidget {
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: ListViewSeparate(
+      home: ListViewMenu(
 
       ),
     );
